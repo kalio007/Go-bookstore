252287be8b2ac81abcb6c30463ad66d0e013e8a4
kalio007 <kalioprince1@gmail.com>
2024-01-18 22:53:31 +0100

    initial commit

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/kalio007/Go-bookstore
+
+go 1.20
+
+require (
+	github.com/go-sql-driver/mysql v1.5.0 // indirect
+	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/jinzhu/gorm v1.9.16 // indirect
+)
---
+++ pkg/config/app.go
@@ -0,0 +1,21 @@
+package config
+
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+var (
+	db *gorm.DB
+)
+
+func Connect() {
+	d, err := gorm.Open("mysql", "root:root@tcp(127.0.0.1:3306)/recording?charset=utf8mb4&parseTime=True&loc=Local")
+	if err != nil {
+		panic(err)
+	}
+	db = d
+}
+func GetDB() *gorm.DB {
+	return db
+}
